a9a77416bdfd000e79f1fe4ddf78bbd0e637a10b
Adam Edelman <aedelman@midtowncg.com>
2019-02-17 18:32:30 -0700

    Initial working commit

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var ctx = context.Background()
+var client *github.Client
+
+// a PR will be mergeable if there is AT LEAST 1 review that has been approved,
+// all status checks pass, and it has not already been merged.
+func isPRMergeable(reviews []*github.PullRequestReview, pr *github.PullRequest) bool {
+	prApproved := false
+	for _, review := range reviews {
+		if review.GetState() == "APPROVED" {
+			prApproved = true
+			break
+		}
+	}
+	if !prApproved {
+		fmt.Println("PR is not approved")
+		return false
+	}
+
+	if pr.GetMergeableState() != "clean" {
+		fmt.Println("PR is not in a mergeable state. Check status check or if already merged")
+		return false
+	}
+
+	// Prompt user to merge or not
+	fmt.Print("Type y or yes to merge, anything else to abort: ")
+	var shouldMerge string
+	fmt.Scan(&shouldMerge)
+	shouldMerge = strings.ToLower(shouldMerge)
+
+	return shouldMerge == "y" || shouldMerge == "yes"
+}
+
+func main() {
+
+	urlParts := strings.Split(os.Args[1], "/")
+	owner := urlParts[3]
+	repo := urlParts[4]
+	prNumber, _ := strconv.Atoi(urlParts[6])
+
+	token := os.Getenv("GH_ACCESS_TOKEN")
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tokenContext := oauth2.NewClient(ctx, tokenSource)
+	client = github.NewClient(tokenContext)
+
+	pr, _, err := client.PullRequests.Get(ctx, owner, repo, prNumber)
+	if err != nil {
+		fmt.Println("Request to get PR info failed. Verify PR URL")
+		os.Exit(1)
+	}
+
+	reviews, _, err := client.PullRequests.ListReviews(ctx, owner, repo, prNumber, nil)
+	if err != nil {
+		fmt.Println("Request to get reviews failed. Verify PR URL")
+		os.Exit(1)
+	}
+
+	if !isPRMergeable(reviews, pr) {
+		fmt.Println("Aborting merge")
+		os.Exit(1)
+	}
+
+	_, _, err = client.PullRequests.Merge(ctx, owner, repo, prNumber, "Merging via script", nil)
+	if err != nil {
+		fmt.Println("Merge request failed")
+		os.Exit(1)
+	}
+
+	_, err = client.Git.DeleteRef(ctx, owner, repo, "heads/"+pr.GetHead().GetRef())
+	if err != nil {
+		fmt.Println("Failed to delete PR branch")
+		os.Exit(1)
+	}
+
+	fmt.Println("PR was merged and branch was deleted successfully!")
+}
